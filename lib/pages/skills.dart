f86489c292056153a22cc3c0e3e4b2686ff7b74f
bruc3balo <brucelosis12@gmail.com>
2024-04-02 07:18:29 +0300

    git

--- lib/pages/skills.dart
+++ lib/pages/skills.dart
@@ -120,30 +120,166 @@ class Skills extends StatelessWidget {
             Technologies(
               name: 'Docker',
               brief:
-                  'I have 2 years of experience with docker containerization',
+                  "Docker simplifies application deployment by packaging them with their dependencies into portable containers, enabling consistent and efficient deployment across various environments.",
               url: 'https://www.docker.com',
               startDate: DateTime(2022, DateTime.august),
               abilities: [
-                Ability(
-                  name: 'Application containerization',
+                const Ability(
+                  name: 'Application Dockerfile authoring',
                   brief:
-                      'Containerizing applications to maintain a stable environment',
-                )
+                      "Dockerfile authoring involves creating configuration files for existing applications, enabling containerization for seamless deployment across different environments.",
+                  points: [
+                    "Externalize configurations to separate them from the application code.",
+                    "Define the build steps in a Dockerfile to automate the image creation process.",
+                    "Utilize multistage builds to ensure images are small by including only necessary dependencies.",
+                    "Opt for slim or Alpine images to further minimize the image size while still meeting application requirements.",
+                  ],
+                ),
+                const Ability(
+                  name: 'Creating a private image registry',
+                  brief:
+                      'Using a private container registry involves securely storing Docker images, controlling access, ensuring compliance, enhancing performance, and enabling collaboration within a controlled environment.',
+                  url:
+                      'https://www.digitalocean.com/community/tutorials/how-to-set-up-a-private-docker-registry-on-ubuntu-22-04',
+                  points: [
+                    "Set up basic authentication to authorize selected users to the registry"
+                  ],
+                ),
+                const Ability(
+                  name: 'Testing with docker compose',
+                  brief:
+                      "Utilizing Docker Compose simplifies the setup of testing environments by orchestrating dependencies and services through an easy-to-use command.",
+                  url: 'https://docs.docker.com/compose',
+                  points: [
+                    "Organize individual compose files for separate services to manage them independently.",
+                    "Simplify service deployment by extending other compose files and leveraging dependencies."
+                  ],
+                ),
               ],
             ),
             Technologies(
               name: 'Kubernetes',
               brief:
-                  'I have 1 year of experience with kubernetes container orchestration',
+                  "It's used for automating deployment, scaling, and management of containerized applications, providing a robust and scalable solution for container orchestration. Having dealt with flavours such as Minikube and Microk8s",
               url: 'https://kubernetes.io',
               startDate: DateTime(2024, DateTime.february),
+              abilities: const [
+                Ability(
+                  name: 'Deployment and Scaling',
+                  brief:
+                      "Efficiently orchestrate cluster deployments with optimal replica configurations on datacenter or node infrastructure to ensure adequate resource provisioning for application functionality.",
+                  points: [
+                    "Provisioning pods through deployments and scaling via replica sets",
+                    "Auto-scaling with auto-callers based on metrics like CPU core usage, memory usage, and number of  HTTP requests",
+                  ],
+                ),
+                Ability(
+                  name: "Networking and Traffic Management",
+                  brief:
+                      "Enable seamless pod-to-node communication while efficiently managing traffic distribution across the network.",
+                  points: [
+                    "Exposing pod networks within the cluster using internal services.",
+                    "Enabling external communication with Kubernetes pods through external services and endpoints.",
+                    "Managing traffic routing to pods by utilizing Nginx Ingress with domain-based routing.",
+                    "Provisioning load balancers for efficient distribution of requests across nodes.",
+                  ],
+                ),
+                Ability(
+                  name: "Configuration and Secrets Management",
+                  brief:
+                      "Enhance application security by centrally managing secrets within the cluster, ensuring secure access for all applications requiring them, and safeguarding configurations in the namespace with kubelet authentication.",
+                  points: [
+                    "Managing pod configurations through config maps",
+                    "Safely managing pod and application secrets stored securely and encoded in Git repositories with enhanced Git security.",
+                  ],
+                ),
+                Ability(
+                  name: "Storage Management",
+                  brief:
+                      "Ensure pod volume storage integrity and consistency by provisioning claimed volumes through kubelet-managed mechanisms.",
+                  points: [
+                    "Dynamically provisioning volumes in response to persistent volume claim requests using storage classes."
+                  ],
+                ),
+                Ability(
+                  name: "Resource Allocation and Optimization:",
+                  brief:
+                      "Optimize resource allocation by assessing application needs and preparing environments accordingly, leveraging horizontal scaling to dynamically adjust resource allocation for optimal efficiency.",
+                  points: [
+                    "Allocating pods to environments with required resources using resource requests to prevent resource starvation.",
+                    "Applying resource quotas to regulate total resource usage."
+                  ],
+                ),
+                Ability(
+                  name: "Namespace Management",
+                  brief:
+                      "Maximize server resource utilization by sharing available resources while segmenting Kubernetes resources to only grant visibility to applications that require them.",
+                  points: [
+                    "Creating namespaces to effectively segment applications and resources for improved organization.",
+                  ],
+                ),
+                Ability(
+                  name: "Deployment Strategies and Upgrade Handling",
+                  brief:
+                      "Ensure continuous application uptime by seamlessly creating upgraded pods and resources to replace the previous ones during deployments.",
+                  points: [
+                    "Upgrading pods seamlessly with zero downtime using rolling updates and node pools.",
+                  ],
+                ),
+                Ability(
+                  name: "Monitoring and Health Checks",
+                  brief:
+                      "Ensure consistent pod availability and traffic flow by conducting continuous health checks, directing traffic only to pods that are up and ready to receive it, and probing when pods are prepared to handle requests.",
+                  points: [
+                    "Implementing probes such as readiness, liveness, and health checks using TCP, HTTP, and Command methods, and configuring delays, timeouts, and thresholds.",
+                  ],
+                ),
+                Ability(
+                  name: "Pod Termination",
+                  brief:
+                      "Safely manage application termination signals within Kubernetes to gracefully conclude transactions, preventing data corruption and ensuring secure shutdown procedures.",
+                  points: [
+                    "Safely terminating pods by responding to signals like preStopHook, sigterm, and sigkill."
+                  ],
+                ),
+              ],
+            ),
+            Technologies(
+              name: 'Git',
+              brief: 'brief',
+              startDate: DateTime(2017, DateTime.august),
+              url: 'https://github.com',
               abilities: [
                 Ability(
-                  name: 'Application Deployment',
-                  brief: 'Deploying services and pods',
-                )
+                    name: 'Manage repositories',
+                    brief: 'CRUD oprations on repositories',
+                    points: [
+                      "Create repositories",
+                      "Clone repositories",
+                      "Update local and remote repositories",
+                    ]),
+                Ability(
+                  name: 'Manage branch repositories',
+                  brief: 'operations on branches',
+                  points: [
+                    "Merge branch repositories",
+                    "Rebase branch repositores",
+                    "Checkout branch repositories",
+                    "Recover branch repositories",
+                    "Fast Forward branch repositories",
+                  ],
+                ),
+                Ability(
+                  name: 'Manage commits on branches',
+                  brief: 'operations for commits',
+                  points: [
+                    "Checkout on commits in branches",
+                    "Cherry pich commits in branches",
+                    "Create new branches from commits",
+                  ],
+                ),
               ],
-            )
+            ),
           ],
         ),
         SkillBoard(
